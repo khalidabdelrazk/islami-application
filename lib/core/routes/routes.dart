4c4ef1b76645fcd9c67f9ddf22646cf87c57ddf0
khalid <khalidabdelrazk4@gmail.com>
2025-03-29 07:02:56 +0200

    Changing NdkVersion to 27 Create Splash Screen Add used Images

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:islami_app/core/routes/route_names.dart';
+import 'package:islami_app/screens/home%20screen/home_screen.dart';
 import 'package:islami_app/screens/splash%20screen/splash_screen.dart';
 
 class Routes {
   static Map<String, Widget Function(BuildContext)> routes = {
     RouteNames.splash: (_) => SplashScreen(),
+    RouteNames.homeScreen: (_) => HomeScreen(),
   };
 }
